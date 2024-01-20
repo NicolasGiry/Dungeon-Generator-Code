785299591612d50c3adc13d12935ff9bb5dd9f26
Drocolas <114723956+NicolasGiry@users.noreply.github.com>
2024-01-20 10:29:43 +0100

    Update RoomManager.cs

--- RoomManager.cs
+++ RoomManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class RoomManager : MonoBehaviour
 {
@@ -14,23 +15,40 @@ public class RoomManager : MonoBehaviour
     GameObject[] tiles;
     public int seed;
     bool personnalSeed;
+    public float distanceChairTable = 0.5f;
+    public float distanceWallTorchX = 0.1f;
+    public float distanceWallTorchZ = 0.1f;
+    public float torchHigh = 0.5f;
 
-    public Slider sliderCorridorWidth;
+    public UnityEngine.UI.Slider sliderCorridorWidth;
     public TextMeshProUGUI corridorWidthText;
 
-    public Slider sliderMaxRooms;
+    public UnityEngine.UI.Slider sliderMaxRooms;
     public TextMeshProUGUI maxRoomsText;
 
-    public Slider sliderMinSize;
+    public UnityEngine.UI.Slider sliderMinSize;
     public TextMeshProUGUI minSizeText;
 
-    public Slider sliderMaxSize;
+    public UnityEngine.UI.Slider sliderMaxSize;
     public TextMeshProUGUI maxSizeText;
 
     public TMP_InputField seedText;
-    public Toggle personnalSeedCheck;
+    public UnityEngine.UI.Toggle personnalSeedCheck;
 
     public PlayerMovement playerMovement;
+
+    public float propsDensity;
+    public GameObject[] propsPref;
+    public GameObject[] columnsPref;
+    public GameObject[] rocksPref;
+    public GameObject chestPref;
+    public GameObject tablePref;
+    public GameObject chairPref;
+    public GameObject torchPref;
+    public GameObject[] props;
+    private int nbProps = 0;
+
+    public TorchManager torchManager;
 
     int nbTiles;
     void Awake()
@@ -63,6 +81,7 @@ public class RoomManager : MonoBehaviour
         seedText.interactable = false;
         seedText.characterLimit = 9;
 
+        props = new GameObject[10000];
         Init();
     }
 
@@ -85,7 +104,9 @@ public class RoomManager : MonoBehaviour
         DestroyScene();
         GenerateSeed();
         nbRooms = 0;
+        nbProps = 0;
         rooms = new Room[maxRooms];
+        torchManager.GenerateTorchColor();
         for (int i = 0; i < maxRooms; i++)
         {
             rooms[nbRooms] = Instantiate(roomPref, Vector3.zero, Quaternion.identity).GetComponent<Room>();
@@ -107,7 +128,136 @@ public class RoomManager : MonoBehaviour
         FindNearestRooms();
         FillScene();
         CreateCorridors();
+        PlaceProps();
+        PlaceChest();
     }
+
+    private void PlaceProps()
+    {
+        int nbPoints = (int) Mathf.Ceil(nbRooms*propsDensity);
+        int roomIndex, nbPropsToPlace;
+        Vector3 roomPos, roomSize;
+        for (int i=0; i<nbPoints; i++)
+        {
+            roomIndex = i;
+
+            roomPos = rooms[roomIndex].GetPos();
+            roomSize = rooms[roomIndex].GetSize();
+
+            PlaceRocks(roomPos, roomSize);
+            PlaceColumns(roomPos, roomSize);
+            PlaceTorches(roomPos, roomSize);
+            if (Random.Range(0,4) == 0)
+            {
+                PlaceTables(roomPos, roomSize);
+            }
+
+            nbPropsToPlace = Random.Range(3, (int) roomSize.x); // faire en fonction de la taille de la salle
+            Vector3 propsPos = new Vector3(roomPos.x + Random.Range(roomSize.x / 4, roomSize.x- roomSize.x / 4), 1f, roomPos.z + Random.Range(roomSize.z / 4, roomSize.z- roomSize.z / 4));
+
+            for (int j=0; j<nbPropsToPlace; j++)
+            {
+                Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+                Vector3 changePos = new Vector3(Random.Range(-1f*roomSize.x/4, 1f * roomSize.x / 4), 0, Random.Range(-1f * roomSize.z / 4, 1f * roomSize.z / 4));
+                props[nbProps] = Instantiate(propsPref[Random.Range(0, propsPref.Length)], propsPos + changePos, randomRotation);
+                nbProps++;
+            }
+        }
+    }
+
+    private void PlaceRocks(Vector3 roomPos, Vector3 roomSize)
+    {
+        int nbRocks = Random.Range(5, (int) roomSize.x*2);
+        for (int i=0; i<nbRocks;i++)
+        {
+            Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+            Vector3 rocksPos = new Vector3(roomPos.x + Random.Range(0, roomSize.x-1), -0.1f, roomPos.z + Random.Range(0, roomSize.z-1));
+            props[nbProps] = Instantiate(rocksPref[Random.Range(0, rocksPref.Length)], rocksPos, randomRotation);
+            nbProps++;
+        }
+    }
+
+    private void PlaceColumns(Vector3 roomPos, Vector3 roomSize)
+    {
+        int nbColumns = Random.Range(0, 5);
+        Vector3[] columnsPos = { new(roomPos.x+.53f, 1f, roomPos.z+.53f), new(roomPos.x+.53f, 1f, roomPos.z + roomSize.z-1.53f),
+            new(roomPos.x + roomSize.x-1.53f, 1f, roomPos.z+.53f), new(roomPos.x + roomSize.x - 1.53f, 1f, roomPos.z + roomSize.z-1.53f)};
+        for (int i = 0; i < nbColumns; i++)
+        {
+            Vector3 columnPos = columnsPos[i];
+            props[nbProps] = Instantiate(columnsPref[Random.Range(0, columnsPref.Length)], columnPos, Quaternion.identity);
+            nbProps++;
+        }
+    }
+
+    void PlaceTorches(Vector3 roomPos, Vector3 roomSize)
+    {
+        int nbTorchX = (int) roomSize.x / 5;
+        int nbTorchZ = (int)roomSize.z / 5;
+
+        for (int i=0; i<nbTorchX; i++)
+        {
+            Quaternion rot = Quaternion.Euler(0f, 0f, 0f);
+            Vector3 pos = new Vector3(roomPos.x + (roomSize.x / (nbTorchX + 1)) * (i + 1), torchHigh, roomPos.z + distanceWallTorchZ);
+            props[nbProps] = Instantiate(torchPref, pos, rot);
+            nbProps++;
+            rot = Quaternion.Euler(0f, 180f, 0f);
+            pos = new Vector3(roomPos.x + (roomSize.x / (nbTorchX + 1)) * (i + 1), torchHigh, roomPos.z + distanceWallTorchX + roomSize.z - 1.53f);
+            props[nbProps] = Instantiate(torchPref, pos, rot);
+            nbProps++;
+        }
+
+        for (int i = 0; i < nbTorchZ; i++)
+        {
+            Quaternion rot = Quaternion.Euler(0f, 90f, 0f);
+            Vector3 pos = new Vector3(roomPos.x + distanceWallTorchZ, torchHigh, roomPos.z + (roomSize.z / (nbTorchZ + 1)) * (i + 1));
+            props[nbProps] = Instantiate(torchPref, pos, rot);
+            nbProps++;
+            rot = Quaternion.Euler(0f, 270f, 0f);
+            pos = new Vector3(roomPos.x + distanceWallTorchX + roomSize.x - 1.53f, torchHigh, roomPos.z + (roomSize.z / (nbTorchZ + 1)) * (i + 1));
+            props[nbProps] = Instantiate(torchPref, pos, rot);
+            nbProps++;
+        }
+    }
+
+    void PlaceTables(Vector3 roomPos, Vector3 roomSize)
+    {
+        Vector3 tablePos = new Vector3(roomPos.x + Random.Range(0, roomSize.x - roomSize.x / 4), 1f, roomPos.z + Random.Range(0, roomSize.z - roomSize.z / 4));
+        Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+        props[nbProps] = Instantiate(tablePref, tablePos, randomRotation);
+        nbProps++;
+
+        // place chairs
+        Vector3[] chairPos = { tablePos + new Vector3(distanceChairTable, 0, distanceChairTable), tablePos + new Vector3(distanceChairTable, 0, -distanceChairTable), 
+            tablePos + new Vector3(-distanceChairTable, 0, distanceChairTable), tablePos + new Vector3(-distanceChairTable, 0, -distanceChairTable) };
+        int nbChairs = Random.Range(1, 5);
+        for (int i = 0; i < nbChairs; i++)
+        {
+            randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+            props[nbProps] = Instantiate(chairPref, chairPos[i], randomRotation);
+            nbProps++;
+        }
+    }
+
+    private void PlaceChest()
+    {
+        int chestRoom = Random.Range(1, nbRooms);
+        props[nbProps] = Instantiate(chestPref, rooms[chestRoom].GetCenter() + new Vector3(0,2,0), Quaternion.identity);
+        nbProps++;
+    }
+
+
+    public void CheckTorch()
+    {
+        for (int i=0; i<nbProps; i++)
+        {
+            if (props[i]!=null && props[i].CompareTag("Torch"))
+            {
+                props[i].GetComponent<DetectWall>().CheckWall();
+            }
+        }
+    }
+
     private void Update()
     {
         personnalSeed = personnalSeedCheck.isOn;
@@ -197,12 +347,18 @@ public class RoomManager : MonoBehaviour
         {
             Destroy(tiles[i]);
         }
+
+        for (int i = 0; i < nbProps; i++)
+        {
+            Destroy(props[i]);
+        }
     }
 
     void FillScene()
     {
         nbTiles = 0;
         tiles = new GameObject[x * y];
+        Quaternion[] tileRot = { Quaternion.Euler(0f, 0f, 0f), Quaternion.Euler(0f, 90f, 0f), Quaternion.Euler(0f, 180f, 0f), Quaternion.Euler(0f, 270f, 0f) };
         for (int i=0; i<x; i++)
         {
             for (int j=0; j<y; j++)
@@ -222,7 +378,8 @@ public class RoomManager : MonoBehaviour
                 }
                 if (freePlace)
                 {
-                    tiles[nbTiles] = Instantiate(tilePref, place, Quaternion.identity);
+                    int rot = Random.Range(0, 4);
+                    tiles[nbTiles] = Instantiate(tilePref, place, tileRot[rot]);
                     nbTiles++;
                 }
             }
@@ -246,7 +403,6 @@ public class RoomManager : MonoBehaviour
 
     void CreateCorridor(Room room1, Room room2)
     {
-        
         Vector3 dir = (room2.transform.position - room1.transform.position).normalized;
         RaycastHit[] hits;
         hits = Physics.SphereCastAll(room1.transform.position, CorridorWidth, dir, room1.distanceNearestRoom);
